cae31d00d042cf82e2f817ee46e4b1dd5040b3fd
unknown <ekananda.bussiness@gmail.com>
2019-08-04 09:47:58 +0700

    first commit

---
+++ app/src/main/java/com/example/youtubeplayer/Config.java
@@ -0,0 +1,10 @@
+package com.example.youtubeplayer;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyApiWmA1BHyQJIVTGo7KDVS-KnzS8Go93M";
+
+}
